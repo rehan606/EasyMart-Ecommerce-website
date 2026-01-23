f308cfb981b533a691d90c6f754d09bed251d7b2
Rehan <rehanlemu@gmail.com>
2026-01-24 00:09:34 +0600

    fix some issues

--- src/Components/HomeComponents/DayofDeal.jsx
+++ src/Components/HomeComponents/DayofDeal.jsx
@@ -15,7 +15,7 @@ const DayofDeal = () => {
 
                     {/* Timer  */} 
                     <div className='flex items-center justify-center gap-4'>
-                        <h2 className="text-md font-semibold">Time End:</h2>
+                        <h2 className="hidden md:block text-md font-semibold">Time End:</h2>
                         <Countdown endTime={new Date().setHours(new Date().getHours() + 20)} />
                     </div>
                 </div>
